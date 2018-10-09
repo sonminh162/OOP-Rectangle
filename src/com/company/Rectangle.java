7dee88bd62e43fb6857cfb05f7b1c4b5376396b3
sonminh162 <32251649+sonminh162@users.noreply.github.com>
2018-10-09 07:33:15 +0700

    finished

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Rectangle {
+    double height,width;
+    public Rectangle(){
+    }
+    public Rectangle(double width,double height){
+        this.width = width;
+        this.height= height;
+    }
+    public double getArea(){
+        return this.width*this.height;
+    }
+    public double getPerimeter(){
+        return (this.width+this.height)*2;
+    }
+    public String display(){
+        return "Rectangle{"+"height="+this.height+"width="+this.width+"}";
+    }
+}
